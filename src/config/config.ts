5bdf758a331f7b908665705351ac80dc2b440d05
Mnk242 <rsharma@traversia.net>
2024-10-03 11:13:00 +0530

    fix: remove unused code form config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,14 +9,3 @@ export function MongoUrl() {
     return "mongodb+srv://rsharma:PDtLo80mXBoJZOoi@cluster0.uso8d.mongodb.net/tbo?retryWrites=true&w=majority&appName=Cluster0";
     // return `mongodb://localhost:27017/tbo`;
 }
-
-export function getBaseUrl(vendorCode: string) {
-    switch (vendorCode) {
-        case '1A':
-            return 'http://localhost:5000'; // dev     
-        case '6E':
-            return 'http://localhost:5001';   //dev     tcilapi.traversia.net:5001 
-    }
-
-
-}
