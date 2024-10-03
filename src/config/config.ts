7463d078fda900d655e49edd9bc28896dae3d17b
Mnk242 <rsharma@traversia.net>
2024-10-03 11:11:35 +0530

    fix: remove unused code form config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,7 +8,6 @@ export const env = dotenv;
 export function MongoUrl() {
     return "mongodb+srv://rsharma:PDtLo80mXBoJZOoi@cluster0.uso8d.mongodb.net/tbo?retryWrites=true&w=majority&appName=Cluster0";
     // return `mongodb://localhost:27017/tbo`;
-    // return `mongodb+srv://hsbhandari:DDUTUgxbWLOvd5Of@tcil.a6wowsv.mongodb.net/microservices`;
 }
 
 export function getBaseUrl(vendorCode: string) {
