e85c58c691504377e945bd00d57c04b4bb37c4fc
Mnk242 <rsharma@traversia.net>
2024-10-15 16:33:30 +0530

    fix: add local db uri

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,6 @@ dotenv.config({
 })
 export const env = dotenv;
 export function MongoUrl() {
-    return "mongodb+srv://rsharma:PDtLo80mXBoJZOoi@cluster0.uso8d.mongodb.net/tbo?retryWrites=true&w=majority&appName=Cluster0";
-    // return `mongodb://localhost:27017/tbo`;
+    // return "mongodb+srv://rsharma:PDtLo80mXBoJZOoi@cluster0.uso8d.mongodb.net/tbo?retryWrites=true&w=majority&appName=Cluster0";
+    return `mongodb://localhost:27017/tbo`;
 }
