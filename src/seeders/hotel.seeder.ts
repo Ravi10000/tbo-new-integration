6dfa292fcc62bce1fee0a53821bf8eca2c22639c
Mnk242 <rsharma@traversia.net>
2024-10-15 16:31:16 +0530

    fix: add hotel seeder

--- src/seeders/hotel.seeder.ts
+++ src/seeders/hotel.seeder.ts
@@ -5,59 +5,96 @@ import StaticHotel from "../models/static-hotel.model";
 import { TBO, TBO_ENDPOINTS } from "../utils/tbo.req";
 import fs from 'fs';
 import path from 'path';
-const filePath = path.resolve(__dirname, "saved-hotel-codes.json");
+const filePath = path.resolve(__dirname);
 console.log({ filePath })
 export async function hotelSeeder() {
-    let savedHotelCodes = JSON.parse(fs.readFileSync(filePath, "utf8"))?.savedHotelCodes;
-    console.log({ savedHotelCodes });
+    let savedHotelCodes = JSON.parse(fs.readFileSync(`${filePath}/saved-hotel-codes.json`, "utf8"))?.savedHotelCodes;
     try {
         await connectionMongoDb(MongoUrl());
-        // const doesHotelsExits = await StaticHotel.exists({});
-        // if (doesHotelsExits) throw new Error("Static Hotel already exists");
-        // const { data: hotels } = await TBO.get(TBO_ENDPOINTS.TEST.TBO_HOTEL_CODES);
-        // const hotelCodes = hotels.map((hotel: { HotelCode: string }) => hotel.HotelCode);
-        // const hotelCodesLength = hotelCodes?.length;
-        // console.log({ hotelCodesLength })
-        // console.log({ originalCodesLength: hotelCodes.HotelCodes?.length });
-        // const savedCodes = await StaticHotel.distinct("HotelCode");
-        // console.log({ savedCodesLength: savedCodes?.length });
-        // let totalMatched = 0;
-        // const newCodes = [...hotelCodes.HotelCodes].filter((code: string) => {
-        //     const isThere = savedCodes.includes(String(code)) || savedCodes.includes(code);
-        //     if (isThere) totalMatched++;
-        //     return !isThere;
-        // });
-        const cities = await City.find();
-        for (let city of cities) {
-            const { data: hotelCodesResponse } = await TBO.post(TBO_ENDPOINTS.TEST.TBO_HOTEL_CODES, { CityCode: city.Code, IsDetailedResponse: false });
-            const hotelCodes = hotelCodesResponse?.Hotels?.map((hotel: { HotelCode: string }) => hotel.HotelCode);
-            const hotelCodesLength = hotelCodes?.length;
-            console.log({ hotelCodesLength });
-            let [from, to] = [0, 100];
-
-            if (!hotelCodesLength) {
-                console.log(`city codes not found for ${city.Code} - ${city.Name}`)
-                continue;
-            }
-            while (from < hotelCodesLength) {
-                console.log({ from, to, hotelCodesLength });
-                const codeList = hotelCodes.slice(from, to).filter((code: string) => !savedHotelCodes.includes(code));
-                if (!codeList.length) {
-                    from = to;
-                    to += 100;
-                    continue;
-                }
-                const currentCodes = codeList.join(",");
-                console.log(`fetching hotel details from ${from}-${to}`);
-                const { data: hotelResponse } = await TBO.post(TBO_ENDPOINTS.TEST.HOTEL_DETAILS, { HotelCodes: currentCodes, Language: "EN" });
-                await StaticHotel.insertMany(hotelResponse.HotelDetails);
-                savedHotelCodes = [...savedHotelCodes, ...codeList];
-                fs.writeFileSync(filePath, JSON.stringify({ savedHotelCodes }, null, 2));
-                console.log(`saved hotels for ${from} - ${to} for city ${city.Name} with city code ${city.Code} `);
+        const staticHotelsLength = await StaticHotel.countDocuments();
+        if (!savedHotelCodes?.length || savedHotelCodes.length !== staticHotelsLength) {
+            const allHotels = await StaticHotel.find();
+            savedHotelCodes = allHotels.map((hotel) => hotel.HotelCode);
+            fs.writeFileSync(`${filePath}/saved-hotel-codes.json`, JSON.stringify({ savedHotelCodes }, null, 2));
+        }
+        console.log({ savedHotelCodesLength: savedHotelCodes.length });
+        const savedCodesMap = savedHotelCodes.reduce((map: {}, code: string) => ({ ...map, [code]: true }), {});
+        const { data: hotelCodeResponse } = await TBO.get(TBO_ENDPOINTS.TEST.HOTEL_CODES);
+        const hotelCodes = hotelCodeResponse.HotelCodes;
+        const hotelCodesLength = hotelCodes?.length;
+        console.log({ hotelCodesLength });
+        // const unsavedHotelCodes = hotelCodes.filter((code: string) => !savedHotelCodes.includes(code));
+        const unsavedHotelCodes = hotelCodes.filter((code: string) => !savedCodesMap[code]);
+        console.log({
+            unsavedHotelCodesLength: unsavedHotelCodes.length,
+            savedHotelCodesLength: savedHotelCodes.length
+        });
+        let [from, to] = [0, 100];
+        // ? all codes
+        while (from <= unsavedHotelCodes?.length) {
+            console.log({ from, to });
+            const codeList = unsavedHotelCodes.slice(from, to);
+            if (!codeList.length) {
                 from = to;
                 to += 100;
+                continue;
             }
+            const currentCodes = codeList.join(",");
+            console.log(`fetching hotel details from ${from}-${to}`);
+            try {
+                const { data: hotelResponse } = await TBO.post(TBO_ENDPOINTS.TEST.HOTEL_DETAILS, { HotelCodes: currentCodes, Language: "EN" });
+                if (hotelResponse.HotelDetails?.length) {
+                    await StaticHotel.insertMany(hotelResponse.HotelDetails);
+                    savedHotelCodes = [...savedHotelCodes, ...codeList];
+                    fs.writeFileSync(`${filePath}/saved-hotel-codes.json`, JSON.stringify({ savedHotelCodes }, null, 2));
+                    console.log(`saved hotels for ${from} - ${to}`);
+                } else {
+                    console.log(`no hotels found`);
+                }
+            } catch (error: any) {
+                console.log({ error });
+                fs.writeFileSync(`${filePath}/${Date.now()}-error.json`, JSON.stringify({ error: error.message, from, to, currentCodes }, null, 2));
+            }
+            from = to;
+            to += 100;
         }
+
+        // ? city vise
+        // const cities = await City.find();
+        // for (let city of cities) {
+        //     const { data: hotelCodesResponse } = await TBO.post(TBO_ENDPOINTS.TEST.TBO_HOTEL_CODES, { CityCode: city.Code, IsDetailedResponse: false });
+        //     const hotelCodes = hotelCodesResponse?.Hotels?.map((hotel: { HotelCode: string }) => hotel.HotelCode);
+        //     const hotelCodesLength = hotelCodes?.length;
+        //     console.log({ hotelCodesLength });
+        //     let [from, to] = [0, 100];
+
+        //     if (!hotelCodesLength) {
+        //         console.log(`city codes not found for ${city.Code} - ${city.Name}`)
+        //         continue;
+        //     }
+        //     while (from <= hotelCodesLength) {
+        //         console.log({ from, to, hotelCodesLength });
+        //         const codeList = hotelCodes.slice(from, to).filter((code: string) => !savedHotelCodes.includes(code));
+        //         if (!codeList.length) {
+        //             from = to;
+        //             to += 100;
+        //             continue;
+        //         }
+        //         const currentCodes = codeList.join(",");
+        //         console.log(`fetching hotel details from ${from}-${to}`);
+        //         const { data: hotelResponse } = await TBO.post(TBO_ENDPOINTS.TEST.HOTEL_DETAILS, { HotelCodes: currentCodes, Language: "EN" });
+        //         if (hotelResponse.HotelDetails?.length) {
+        //             await StaticHotel.insertMany(hotelResponse.HotelDetails);
+        //             savedHotelCodes = [...savedHotelCodes, ...codeList];
+        //             fs.writeFileSync(filePath, JSON.stringify({ savedHotelCodes }, null, 2));
+        //             console.log(`saved hotels for ${from} - ${to} for city ${city.Name} with city code ${city.Code} `);
+        //         } else {
+        //             console.log(`no hotels found for city ${city.Name} with city code ${city.Code}`);
+        //         }
+        //         from = to;
+        //         to += 100;
+        //     }
+        // }
     } catch (error) {
         console.log({ error });
     } finally {
@@ -66,34 +103,31 @@ export async function hotelSeeder() {
     }
 }
 
-// async function removeDuplicates() {
-//     let totalDuplicates = 0;
-//     try {
-//         await connectionMongoDb(MongoUrl());
-//         const hotelCodes = await StaticHotel.find().distinct("HotelCode");
-//         console.dir({ hotelCodes: hotelCodes.length }, { depth: null });
-//         for (let code of hotelCodes) {
-//             const hotels = await StaticHotel.find({ HotelCode: code });
-//             if (hotels.length > 1) {
-//                 console.log(`found ${hotels.length - 1} duplicates`);
-//                 const hotelsToRemove = [];
-//                 for (let i = 1; i < hotels.length; i++)
-//                     hotelsToRemove.push(hotels[i]._id);
-//                 totalDuplicates += hotelsToRemove.length;
-//                 // await StaticHotel.deleteMany({ _id: { $in: hotelsToRemove } });
-//                 console.log(`removed ${hotelsToRemove.length} hotels`);
-//             }
-//         }
+async function removeDuplicates() {
+    let totalDuplicates = 0;
+    try {
+        await connectionMongoDb(MongoUrl());
+        const hotelCodes = await StaticHotel.find().distinct("HotelCode");
+        console.dir({ hotelCodes: hotelCodes.length }, { depth: null });
+        for (let code of hotelCodes) {
+            const hotels = await StaticHotel.find({ HotelCode: code });
+            if (hotels.length > 1) {
+                console.log(`found ${hotels.length - 1} duplicates`);
+                const hotelsToRemove = [];
+                for (let i = 1; i < hotels.length; i++)
+                    hotelsToRemove.push(hotels[i]._id);
+                totalDuplicates += hotelsToRemove.length;
+                // await StaticHotel.deleteMany({ _id: { $in: hotelsToRemove } });
+                console.log(`removed ${hotelsToRemove.length} hotels`);
+            }
+        }
 
-//     } catch (error) {
-//         console.log({ error })
-//     } finally {
-//         console.log({ totalDuplicates })
-//         process.exit();
-//     }
-// }
-// removeDuplicates();
-
-
+    } catch (error) {
+        console.log({ error })
+    } finally {
+        console.log({ totalDuplicates })
+        process.exit();
+    }
+}
 
 hotelSeeder();
